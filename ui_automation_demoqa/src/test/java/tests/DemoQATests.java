8218e576826052183ce4c482195fedec47bcc1ac
chandanvars <139095565+chandanvars@users.noreply.github.com>
2025-04-15 21:49:02 +0530

    Convert DemoQATests to Playwright

--- ui_automation_demoqa/src/test/java/tests/DemoQATests.java
+++ ui_automation_demoqa/src/test/java/tests/DemoQATests.java
@@ -1,19 +1,20 @@
 package tests;
 
-import base.BaseTest;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.*;
+import playwright.PlaywrightManager;
 import utils.DataProviderUtil;
 
-public class DemoQATests extends BaseTest {
+public class DemoQATests {
 
     @Test(dataProvider = "textBoxDataProvider", dataProviderClass = DataProviderUtil.class)
     public void testTextBox(String fullName, String email, String currentAddress, String permanentAddress) {
-        HomePage homePage = new HomePage(driver);
+        PlaywrightManager.initialize();
+        HomePage homePage = new HomePage();
         homePage.clickOnElementsCard();
 
-        TextBoxPage textBoxPage = new TextBoxPage(driver);
+        TextBoxPage textBoxPage = new TextBoxPage();
         textBoxPage.enterFullName(fullName);
         textBoxPage.enterEmail(email);
         textBoxPage.enterCurrentAddress(currentAddress);
@@ -21,62 +22,72 @@ public class DemoQATests extends BaseTest {
         textBoxPage.clickSubmit();
 
         Assert.assertTrue(textBoxPage.isOutputSectionDisplayed(), "Output section is not displayed");
+        PlaywrightManager.close();
     }
 
     @Test
     public void testButtonClick() {
-        HomePage homePage = new HomePage(driver);
+        PlaywrightManager.initialize();
+        HomePage homePage = new HomePage();
         homePage.clickOnElementsCard();
 
-        ButtonsPage buttonsPage = new ButtonsPage(driver);
+        ButtonsPage buttonsPage = new ButtonsPage();
         buttonsPage.doubleClickButton();
 
         Assert.assertEquals(buttonsPage.getDoubleClickMessage(), "You have done a double click", "Double click message is incorrect");
+        PlaywrightManager.close();
     }
 
     @Test
     public void testCheckBox() {
-        HomePage homePage = new HomePage(driver);
+        PlaywrightManager.initialize();
+        HomePage homePage = new HomePage();
         homePage.clickOnElementsCard();
 
-        CheckBoxPage checkBoxPage = new CheckBoxPage(driver);
+        CheckBoxPage checkBoxPage = new CheckBoxPage();
         checkBoxPage.expandAll();
         checkBoxPage.selectHomeCheckBox();
 
         Assert.assertTrue(checkBoxPage.isResultDisplayed(), "Result section is not displayed");
+        PlaywrightManager.close();
     }
 
     @Test
     public void testRadioButton() {
-        HomePage homePage = new HomePage(driver);
+        PlaywrightManager.initialize();
+        HomePage homePage = new HomePage();
         homePage.clickOnElementsCard();
 
-        RadioButtonPage radioButtonPage = new RadioButtonPage(driver);
+        RadioButtonPage radioButtonPage = new RadioButtonPage();
         radioButtonPage.selectYesRadioButton();
 
         Assert.assertEquals(radioButtonPage.getSuccessMessage(), "Yes", "Radio button selection message is incorrect");
+        PlaywrightManager.close();
     }
 
     @Test
     public void testAlert() {
-        HomePage homePage = new HomePage(driver);
+        PlaywrightManager.initialize();
+        HomePage homePage = new HomePage();
         homePage.clickOnElementsCard();
 
-        AlertsPage alertsPage = new AlertsPage(driver);
+        AlertsPage alertsPage = new AlertsPage();
         alertsPage.clickAlertButton();
 
         String alertText = alertsPage.getAlertText();
         alertsPage.acceptAlert();
 
         Assert.assertEquals(alertText, "This is a test alert", "Alert text is incorrect");
+        PlaywrightManager.close();
     }
 
     @Test
     public void testWebTable() {
-        HomePage homePage = new HomePage(driver);
+        PlaywrightManager.initialize();
+        HomePage homePage = new HomePage();
         homePage.clickOnElementsCard();
 
-        WebTablePage webTablePage = new WebTablePage(driver);
+        WebTablePage webTablePage = new WebTablePage();
         webTablePage.clickAddButton();
         webTablePage.enterFirstName("John");
         webTablePage.enterLastName("Doe");
@@ -87,16 +98,19 @@ public class DemoQATests extends BaseTest {
         webTablePage.clickSubmitButton();
 
         Assert.assertTrue(webTablePage.isTableRowDisplayed(), "New table row is not displayed");
+        PlaywrightManager.close();
     }
 
     @Test
     public void testDynamicProperties() throws InterruptedException {
-        HomePage homePage = new HomePage(driver);
+        PlaywrightManager.initialize();
+        HomePage homePage = new HomePage();
         homePage.clickOnElementsCard();
 
-        DynamicPropertiesPage dynamicPropertiesPage = new DynamicPropertiesPage(driver);
+        DynamicPropertiesPage dynamicPropertiesPage = new DynamicPropertiesPage();
         Thread.sleep(5000); // Wait for 5 seconds for the button to become clickable
 
         Assert.assertTrue(dynamicPropertiesPage.isEnableAfterButtonClickable(), "Button is not clickable after 5 seconds");
+        PlaywrightManager.close();
     }
 }
